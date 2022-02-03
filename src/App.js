8e8e0b2d2f8626b35ab4f1279c38690796b6125f
Ivan Popovic <ivanivopop@gmail.com>
2022-02-03 14:25:33 +0100

    set up react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-function App() {
-   return <div></div>
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+
+const App = () => {
+   return (
+      <div>
+         <Routes>
+            <Route path='/' element={<HomePage />}></Route>
+         </Routes>
+      </div>
+   )
 }
 
 export default App
